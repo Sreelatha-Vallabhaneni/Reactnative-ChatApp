eae3766a60fc47f057764e988d8d6fe16be0c13b
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-08-20 10:16:14 +0200

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from './screens/loginScreen';
+import ChatScreen from './screens/chatScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Welcome" }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{ title: "hello" }}
+        />
+        {/* <Stack.Screen name="Profile" component={} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ screens/loginScreen.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
+const LoginScreen = ({ navigation }) => {
+    const [input ,setInput ] = useState('');
+    const Continue = () => {
+      navigation.navigate("Chat", input);
+    }
+  return (
+      <View style={styles.container}>
+          <View style={styles.circle} />
+          <View style={{marginTop: 64}}>
+              <Image source={require("../assets/chat-image.png")}
+              style={{width:100, height: 100, alignSelf:"center"}}/>
+          </View>
+          <View style={{marginHorizontal:32}}>
+              <Text styles={styles.header}>UserName</Text>
+              <TextInput style={styles.input} placeholder="Enter Username" onChangeText={name=> setInput(name)} value={input} />
+              <View style={{alignSelf: "flex-end", marginTop: 64}}>
+                  <TouchableOpacity style= {styles.continue} onPress={Continue}>
+                      <Ionicons name="md-arrow-round-forward" size={24} color="#FFF" />
+                  </TouchableOpacity>
+              </View>
+          </View>
+      </View>
+      
+    // <Button
+    //   title="Go to Jane's profile"
+    //   onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+    // />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F5F7",
+  },
+  circle: {
+    width: 500,
+    height: 500,
+    borderRadius: 500 / 2,
+    backgroundColor: "#FFF",
+    position: "absolute",
+    left: -120,
+    top: -20,
+  },
+  header: {
+    fontWeight: "800",
+    fontSize: 30,
+    color: "#514E5A",
+    marginTop: 32,
+  },
+  input: {
+      marginTop: 32,
+      height: 50,
+      borderWidth: StyleSheet.hairlineWidth,
+      borderColor: "#BAB7C3",
+      borderRadius: 30,
+      paddingHorizontal: 20,
+      color:"#514E5A",
+      fontWeight:"600"
+  },
+  continue: {
+    width: 70,
+    height: 70,
+    borderRadius: 70/2,
+    backgroundColor: "#9075E3",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default LoginScreen;
