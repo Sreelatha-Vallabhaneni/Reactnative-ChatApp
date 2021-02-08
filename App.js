8dfbff4136efdf538a9d242e583f976901152854
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2021-02-08 12:24:59 +0100

    deleted commented code

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const App = () => {
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ title: "Welcome" }}
+          options={{ title: "Wel" }}
         />
         <Stack.Screen
           name="Chat"
@@ -28,24 +28,4 @@ const App = () => {
 };
 
 export default App;
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
